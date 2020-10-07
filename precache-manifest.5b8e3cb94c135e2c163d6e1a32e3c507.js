bbc45746bedd23a67bb8d5cd3064d4f93c4578cc
MaximoJunior <studentdeveloper19@gmail.com>
2020-10-07 18:27:01 -0400

    Updates

---
+++ precache-manifest.5b8e3cb94c135e2c163d6e1a32e3c507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5cc1724fe6d4fb57e8bb259b177315",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "7e40ba4f71db070a4f56",
+    "url": "/drum-machine/static/css/main.ecbef5e9.chunk.css"
+  },
+  {
+    "revision": "1e0ae55e4f683b15c137",
+    "url": "/drum-machine/static/js/2.3739f3cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.3739f3cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e40ba4f71db070a4f56",
+    "url": "/drum-machine/static/js/main.6b66c4b5.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
